f8949f03d95b3fb150e399b67d254da7c441e5bd
Sparrow <gastritravage@gmail.com>
2023-02-17 19:04:02 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module passbot
+
+go 1.19
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.7
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/telegram-bot-api.v4 v4.6.4
+	gorm.io/driver/postgres v1.4.7 // indirect
+	gorm.io/gorm v1.24.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/base64"
+	"log"
+	"strings"
+	"unicode/utf8"
+
+	_ "github.com/lib/pq"
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://bot:password@db:5432/passbot?sslmode=disable")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI("bot_token")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "save":
+				parts := strings.SplitN(update.Message.Text, " ", 3)
+				if len(parts) != 3 {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Использование: /save [имя ресурса] [пароль]. Например, /save google 123456"))
+					continue
+				}
+
+				resource := parts[1]
+				password := parts[2]
+
+				encodedPassword := base64.StdEncoding.EncodeToString([]byte(password))
+
+				_, err := db.Exec("INSERT INTO passwords (user_id, resource, password) VALUES ($1, $2, $3)", update.Message.From.ID, resource, encodedPassword)
+				if err != nil {
+					log.Panic(err)
+				}
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Пароль успешно сохранен!"))
+
+			case "list":
+				rows, err := db.Query("SELECT resource FROM passwords WHERE user_id = $1", update.Message.From.ID)
+				if err != nil {
+					log.Panic(err)
+				}
+				defer rows.Close()
+
+				var resources []string
+				for rows.Next() {
+					var resource string
+					err := rows.Scan(&resource)
+					if err != nil {
+						log.Panic(err)
+					}
+					resources = append(resources, resource)
+				}
+
+				if len(resources) == 0 {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "У вас нет сохраненных ресурсов."))
+					continue
+				}
+
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Ваши сохраненные ресурсы:\n"+strings.Join(resources, "\n")))
+
+			case "get":
+				parts := strings.SplitN(update.Message.Text, " ", 2)
+				if len(parts) != 2 {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Использование: /get [имя ресурса]. Например, /get google"))
+					continue
+				}
+
+				resource := parts[1]
+				var password string
+				err := db.QueryRow("SELECT password FROM passwords WHERE user_id = $1 AND resource = $2", update.Message.From.ID, resource).Scan(&password)
+				if err != nil {
+					if err == sql.ErrNoRows {
+						bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Пароль для указанного ресурса не найден."))
+					} else {
+						log.Panic(err)
+					}
+					continue
+				}
+
+				decodedPassword, err := base64.StdEncoding.DecodeString(password)
+				if err != nil {
+					log.Panic(err)
+				}
+				decodedPasswordStr := string(decodedPassword)
+				if !utf8.ValidString(decodedPasswordStr) {
+					decodedPasswordStr = string([]rune(decodedPasswordStr))
+				}
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, decodedPasswordStr)
+				msg.ParseMode = "HTML"
+				bot.Send(msg)
+
+			default:
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда. Доступные команды: /save, /get."))
+			}
+		}
+	}
+}
