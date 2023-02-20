7d41cf553645b323b18e8f78e66008d81e824831
Sparrow <gastritravage@gmail.com>
2023-02-21 01:04:21 +0200

    added delete and update case

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot, err := tgbotapi.NewBotAPI("bot_token")
+	bot, err := tgbotapi.NewBotAPI("6045450017:AAGLWRnptfEb2BR34FGfa2u-n-RRhcCqEGo")
 	if err != nil {
 		log.Panic(err)
 	}
@@ -85,6 +85,65 @@ func main() {
 				}
 
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Ваши сохраненные ресурсы:\n"+strings.Join(resources, "\n")))
+
+			case "update":
+				parts := strings.SplitN(update.Message.Text, " ", 4)
+				if len(parts) != 4 {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Использование: /update [имя ресурса] [старый пароль] [новый пароль]. Например, /update google 123456 654321"))
+					continue
+				}
+
+				resource := parts[1]
+				oldPassword := parts[2]
+				newPassword := parts[3]
+
+				var password string
+				err := db.QueryRow("SELECT password FROM passwords WHERE user_id = $1 AND resource = $2", update.Message.From.ID, resource).Scan(&password)
+				if err != nil {
+					if err == sql.ErrNoRows {
+						bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Пароль для указанного ресурса не найден."))
+					} else {
+						log.Panic(err)
+					}
+					continue
+				}
+
+				decodedPassword, err := base64.StdEncoding.DecodeString(password)
+				if err != nil {
+					log.Panic(err)
+				}
+				decodedPasswordStr := string(decodedPassword)
+				if !utf8.ValidString(decodedPasswordStr) {
+					decodedPasswordStr = string([]rune(decodedPasswordStr))
+				}
+
+				if decodedPasswordStr != oldPassword {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Неверный старый пароль."))
+					continue
+				}
+
+				encodedNewPassword := base64.StdEncoding.EncodeToString([]byte(newPassword))
+				_, err = db.Exec("UPDATE passwords SET password = $1 WHERE user_id = $2 AND resource = $3", encodedNewPassword, update.Message.From.ID, resource)
+				if err != nil {
+					log.Panic(err)
+				}
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Пароль успешно обновлен!"))
+
+			case "delete":
+				parts := strings.SplitN(update.Message.Text, " ", 2)
+				if len(parts) != 2 {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Использование: /delete [имя ресурса]. Например, /delete google"))
+					continue
+				}
+
+				resource := parts[1]
+
+				_, err := db.Exec("DELETE FROM passwords WHERE user_id = $1 AND resource = $2", update.Message.From.ID, resource)
+				if err != nil {
+					log.Panic(err)
+				}
+
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Пароль для указанного ресурса успешно удален."))
 
 			case "get":
 				parts := strings.SplitN(update.Message.Text, " ", 2)
