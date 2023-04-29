49a21cd8c5fbd11b3114932d1a12ac3dad24795f
Sparrow <gastritravage@gmail.com>
2023-04-30 02:45:34 +0300

    fix sql srcipt

--- init.sql
+++ init.sql
@@ -1,6 +1,7 @@
-CREATE USER bot123;
-GRANT ALL PRIVILEGES ON DATABASE passbot TO bot123;
-ALTER USER bot123 WITH PASSWORD 'password';
+CREATE DATABASE passbot;
+CREATE USER bot;
+GRANT ALL PRIVILEGES ON DATABASE passbot TO bot;
+ALTER USER bot WITH PASSWORD 'password';
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE passwords (
@@ -10,9 +11,3 @@ CREATE TABLE passwords (
   login TEXT NOT NULL,
   password BYTEA NOT NULL
 );
-
-INSERT INTO passwords (user_id, resource, password)
-VALUES (1, 'example.com', pgp_sym_encrypt('password123', 'my_secret_key'));
-
-SELECT user_id, resource, pgp_sym_decrypt(password, 'my_secret_key') AS password
-FROM passwords;	
